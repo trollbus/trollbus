ac1e04ded13d5017c595c3bacb994b7ffc7ac8bc
Kenny1911 <o-muzyka@mail.ru>
2024-11-03 22:34:17 +0400

    Message interface

---
+++ src/Message/Event.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenny1911\SisyphBus\Message;
+
+/**
+ * @extends Message<void>
+ */
+interface Event extends Message {}
---
+++ src/Message/Message.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenny1911\SisyphBus\Message;
+
+/**
+ * @template-covariant TResult
+ */
+interface Message {}
