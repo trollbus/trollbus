11d57615c550ef425e52b5512f3a47112b20c396
Kenny1911 <o-muzyka@mail.ru>
2025-02-18 23:39:34 +0400

    Update PHP-CS-Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,19 +20,6 @@ $config = (new Config())
     ->setCacheFile(__DIR__ . '/var/' . basename(__FILE__) . '.cache');
 
 (new PhpCsFixerCodingStandard())->applyTo($config, [
-    'final_public_method_for_abstract_class' => false,
-    'no_unset_on_property' => false,
-    /** @see TypeInheritance */
-    'strict_comparison' => false,
-    'logical_operators' => false,
-    'no_multiline_whitespace_around_double_arrow' => false,
-    'class_attributes_separation' => ['elements' => [
-        'trait_import' => 'only_if_meta',
-        'const' => 'only_if_meta',
-        'case' => 'only_if_meta',
-        'property' => 'one',
-        'method' => 'one',
-    ]],
     'yoda_style' => true,
 ]);
 
