3f9692b70a47e1d57e2174597017dc5ad1efe526
Kenny1911 <o-muzyka@mail.ru>
2025-03-01 01:33:10 +0400

    WIP: HandlerRegistryPass

---
+++ src/TrollbusBundle/DependencyInjection/CompilerPass/HandlerRegistryPass.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trollbus\TrollbusBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
+use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Reference;
+use Trollbus\Message\Event;
+use Trollbus\Message\Message;
+use Trollbus\MessageBus\Handler\EventHandler;
+use Trollbus\TrollbusBundle\HandlerRegistry\ContainerHandlerRegistry;
+
+final class HandlerRegistryPass implements CompilerPassInterface
+{
+    use PriorityTaggedServiceTrait;
+
+    /**
+     * @param non-empty-string $prefix
+     */
+    public function __construct(
+        private readonly string $prefix,
+    ) {}
+
+    #[\Override]
+    public function process(ContainerBuilder $container): void
+    {
+        /** @var array<class-string<Message>, Reference> $messageToHandlerMap */
+        $messageToHandlerMap = [];
+
+        foreach ($this->getMessageToHandlerIdsMap($container, $this->prefix . '.handler') as $message => $ids) {
+            if (1 === \count($ids)) {
+                $messageToHandlerMap[$message] = new Reference($ids[0]);
+            }
+
+            if (!is_subclass_of($message, Event::class)) {
+                throw new LogicException(\sprintf('Non-event message %s must have 1 handler, got %s', $message, \count($ids)));
+            }
+
+            $id = \sprintf('%s.event_handler.%s', $this->prefix, $message);
+            $container->setDefinition($id, new Definition(EventHandler::class, [array_map(static fn($handlerId) => new Reference($handlerId), $ids)]));
+        }
+
+        $container->setDefinition(
+            $this->prefix . '.handler_registry',
+            new Definition(
+                ContainerHandlerRegistry::class,
+                [
+                    ServiceLocatorTagPass::register($container, $messageToHandlerMap),
+                ],
+            ),
+        );
+    }
+
+    /**
+     * @return array<class-string<Message>, list<non-empty-string>>
+     */
+    private function getMessageToHandlerIdsMap(ContainerBuilder $container, string $handlerTag): array
+    {
+        /** @var array<class-string<Message>, non-empty-string> $messageToHandlerIdsMap */
+        $messageToHandlerIdsMap = [];
+
+        foreach ($container->getDefinitions() as $id => $definition) {
+            if (!$definition->hasTag($handlerTag)) {
+                continue;
+            }
+
+            foreach ($definition->getTags() as $tag) {
+                $messageClass = $tag['message']
+                    ?? throw new LogicException(\sprintf(
+                        'Service "%s" tagged by "%s" requires tag attribute "%s".',
+                        $id,
+                        $handlerTag,
+                        'message',
+                    ));
+
+                $messageClass = self::getFqcn($messageClass);
+
+                if (!is_subclass_of($messageClass, Message::class)) {
+                    throw new LogicException(\sprintf(
+                        'Service "%s" tagged by "%s" contains invalid message class "%s" in attribute "%s".',
+                        $id,
+                        $handlerTag,
+                        $messageClass,
+                        'message',
+                    ));
+                }
+
+                $messageToHandlerIdsMap[$messageClass][] = $id;
+                $messageToHandlerIdsMap[$messageClass] = array_unique($messageToHandlerIdsMap[$messageClass] ?? []);
+            }
+
+        }
+    }
+
+    /**
+     * @return class-string
+     */
+    private static function getFqcn(string $class): string
+    {
+        try {
+            return (new \ReflectionClass($class))->getName();
+        } catch (\ReflectionException $e) {
+            throw new LogicException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
--- src/TrollbusBundle/TrollbusBundle.php
+++ src/TrollbusBundle/TrollbusBundle.php
@@ -4,6 +4,34 @@ declare(strict_types=1);
 
 namespace Trollbus\TrollbusBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Trollbus\MessageBus\MessageBus;
+use Trollbus\TrollbusBundle\DependencyInjection\CompilerPass\HandlerRegistryPass;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
-final class TrollbusBundle extends Bundle {}
+/**
+ * @psalm-type Config = array{
+ *     service_prefix: non-empty-string
+ * }
+ */
+final class TrollbusBundle extends AbstractBundle
+{
+    #[\Override]
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $serviceName = 'message_bus';
+        // $serviceName = 'trollbus';
+
+        $builder->addCompilerPass(new HandlerRegistryPass($serviceName));
+
+        $container->services()
+            ->set(MessageBus::class)
+                ->args([
+                    service($serviceName . '.handler_registry'),
+                    tagged_iterator($serviceName . '.handler_middleware'),
+                ]);
+    }
+}
