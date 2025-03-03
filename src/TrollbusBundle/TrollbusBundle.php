768398e87423395535c6a402062070f0265d4261
Kenny1911 <o-muzyka@mail.ru>
2025-03-03 22:42:50 +0400

    WIP: TrollbusBundle services definitions

--- src/TrollbusBundle/TrollbusBundle.php
+++ src/TrollbusBundle/TrollbusBundle.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 
 namespace Trollbus\TrollbusBundle;
 
+use Composer\InstalledVersions;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Trollbus\DoctrineORMBridge\EntityHandler\DoctrineEntityFinder;
+use Trollbus\DoctrineORMBridge\EntityHandler\DoctrineEntitySaver;
+use Trollbus\DoctrineORMBridge\Flusher\FlusherMiddleware;
+use Trollbus\DoctrineORMBridge\Transaction\DoctrineTransactionProvider;
 use Trollbus\MessageBus\CreatedAt\CreatedAtMiddleware;
+use Trollbus\MessageBus\EntityHandler\PropertyCriteriaResolver;
 use Trollbus\MessageBus\Logging\LogMiddleware;
 use Trollbus\MessageBus\MessageBus;
 use Trollbus\MessageBus\MessageId\CausationIdMiddleware;
 use Trollbus\MessageBus\MessageId\CorrelationIdMiddleware;
 use Trollbus\MessageBus\MessageId\MessageIdMiddleware;
 use Trollbus\MessageBus\MessageId\RandomMessageIdGenerator;
+use Trollbus\MessageBus\Transaction\WrapInTransactionMiddleware;
 use Trollbus\TrollbusBundle\DependencyInjection\CompilerPass\HandlerRegistryPass;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
@@ -41,14 +49,16 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_it
  *     },
  *     entity_handler: array{
  *         enabled: bool,
- *         entity_filter: non-empty-string|null,
+ *         entity_finder: non-empty-string|null,
  *         entity_saver: non-empty-string|null,
  *         criteria_resolver: non-empty-string|null
  *     },
  *     doctrine_orm_bridge: array{
  *         enabled: bool,
  *         manager_registry: non-empty-string,
- *         manager: non-empty-string|null
+ *         manager: non-empty-string|null,
+ *         entity_saver_flush: bool,
+ *         flusher: bool
  *     }
  * }
  */
@@ -71,29 +81,30 @@ final class TrollbusBundle extends AbstractBundle
 
     /**
      * @psalm-param Config $config
+     * @psalm-suppress MoreSpecificImplementedParamType
      */
     #[\Override]
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        /** @var non-empty-string $prefix */
-        $prefix = $this->extensionAlias;
         $services = $container->services();
 
         $this->loadCreatedAt($config, $services);
         $this->loadLogger($config, $services);
         $this->loadMessageId($config, $services);
+        $this->loadDoctrineOrmBridge($config, $services);
         $this->loadTransaction($config, $services);
         $this->loadEntityHandler($config, $services);
-        $this->loadDoctrineOrmBridge($config, $services);
 
-        $builder->addCompilerPass(new HandlerRegistryPass($prefix));
+        $builder->addCompilerPass(new HandlerRegistryPass('trollbus'));
 
-        $container->services()
+        $container
+            ->services()
             ->set(MessageBus::class)
                 ->args([
-                    service($prefix . '.handler_registry'),
-                    tagged_iterator($prefix . '.middleware'),
-                ]);
+                    service('trollbus.handler_registry'),
+                    tagged_iterator('trollbus.middleware'),
+                ])
+            ->alias('trollbus', MessageBus::class);
     }
 
     private function configureCreatedAt(NodeBuilder $config): void
@@ -104,7 +115,7 @@ final class TrollbusBundle extends AbstractBundle
 
                 ->children()
                     ->stringNode('clock')
-                        ->cannotBeEmpty()->beforeNormalization()->ifTrue()->then()->end()
+                        ->cannotBeEmpty()
                         ->defaultNull();
     }
 
@@ -113,14 +124,16 @@ final class TrollbusBundle extends AbstractBundle
      */
     private function loadCreatedAt(array $config, ServicesConfigurator $services): void
     {
-        if ($config['created_at']['enabled']) {
-            $services
-                ->set(CreatedAtMiddleware::class)
-                    ->args([
-                        isset($config['created_at']['clock']) ? service($config['created_at']['clock']) : null,
-                    ])
-                    ->tag($this->extensionAlias . '.middleware', ['priority' => 1000]);
+        if (false === $config['created_at']['enabled']) {
+            return;
         }
+
+        $services
+            ->set(CreatedAtMiddleware::class)
+                ->args([
+                    isset($config['created_at']['clock']) ? service($config['created_at']['clock']) : null,
+                ])
+                ->tag('trollbus.middleware', ['priority' => 1000]);
     }
 
     private function configureLogger(NodeBuilder $config): void
@@ -139,12 +152,16 @@ final class TrollbusBundle extends AbstractBundle
      */
     private function loadLogger(array $config, ServicesConfigurator $services): void
     {
-        if ($config['logger']['enabled']) {
-            $services
-                ->set(LogMiddleware::class)
-                    ->args([service($config['logger']['logger'])])
-                    ->tag($this->extensionAlias . '.middleware', ['priority' => 900]);
+        if (false === $config['logger']['enabled']) {
+            return;
         }
+
+        $services
+            ->set(LogMiddleware::class)
+                ->args([
+                    service($config['logger']['logger']),
+                ])
+                ->tag('trollbus.middleware', ['priority' => 900]);
     }
 
     private function configureMessageId(NodeBuilder $config): void
@@ -163,27 +180,29 @@ final class TrollbusBundle extends AbstractBundle
      */
     private function loadMessageId(array $config, ServicesConfigurator $services): void
     {
-        if ($config['message_id']['enabled']) {
-            if (null === $config['message_id']['generator']) {
-                $services->set(RandomMessageIdGenerator::class);
-                $generator = service(RandomMessageIdGenerator::class);
-            } else {
-                $generator = service($config['message_id']['generator']);
-            }
-
-            $services
-                ->set(MessageIdMiddleware::class)
-                    ->args([
-                        $generator,
-                    ])
-                    ->tag($this->extensionAlias . '.middleware', ['priority' => 810])
-
-                ->set(CorrelationIdMiddleware::class)
-                    ->tag($this->extensionAlias . '.middleware', ['priority' => 800])
-
-                ->set(CausationIdMiddleware::class)
-                    ->tag($this->extensionAlias . '.middleware', ['priority' => 800]);
+        if (false === $config['message_id']['enabled']) {
+            return;
         }
+
+        if (null === $config['message_id']['generator']) {
+            $services->set(RandomMessageIdGenerator::class);
+            $generator = service(RandomMessageIdGenerator::class);
+        } else {
+            $generator = service($config['message_id']['generator']);
+        }
+
+        $services
+            ->set(MessageIdMiddleware::class)
+                ->args([
+                    $generator,
+                ])
+                ->tag('trollbus.middleware', ['priority' => 810])
+
+            ->set(CorrelationIdMiddleware::class)
+                ->tag('trollbus.middleware', ['priority' => 800])
+
+            ->set(CausationIdMiddleware::class)
+                ->tag('trollbus.middleware', ['priority' => 800]);
     }
 
     private function configureTransaction(NodeBuilder $config): void
@@ -202,7 +221,20 @@ final class TrollbusBundle extends AbstractBundle
      */
     private function loadTransaction(array $config, ServicesConfigurator $services): void
     {
-        // todo
+        if (false === $config['transaction']['enabled']) {
+            return;
+        }
+
+        if (null === $config['transaction']['transaction_provider']) {
+            throw new LogicException('Transaction provider not defined. Please, define config "trollbus.transaction.transaction_provider".');
+        }
+
+        $services
+            ->set(WrapInTransactionMiddleware::class)
+                ->args([
+                    service($config['transaction']['transaction_provider']),
+                ])
+                ->tag('trollbus.middleware', ['priority' => 700]);
     }
 
     private function configureEntityHandler(NodeBuilder $config): void
@@ -211,7 +243,7 @@ final class TrollbusBundle extends AbstractBundle
             ->arrayNode('entity_handler')
             ->canBeEnabled()
             ->children()
-                ->stringNode('entity_filter')
+                ->stringNode('entity_finder')
                     ->cannotBeEmpty()
                     ->defaultNull()
                     ->end()
@@ -229,7 +261,28 @@ final class TrollbusBundle extends AbstractBundle
      */
     private function loadEntityHandler(array $config, ServicesConfigurator $services): void
     {
-        // todo
+        if (false === $config['entity_handler']['enabled']) {
+            return;
+        }
+
+        if (null === $config['entity_handler']['entity_finder']) {
+            throw new LogicException('Entity finder not defined. Please, define config "trollbus.entity_handler.entity_finder".');
+        }
+
+        $services->alias('trollbus.entity_handler.default_entity_finder', $config['entity_handler']['entity_finder']);
+
+        if (null === $config['entity_handler']['entity_saver']) {
+            throw new LogicException('Entity saver not defined. Please, define config "trollbus.entity_handler.entity_saver".');
+        }
+
+        $services->alias('trollbus.entity_handler.default_entity_saver', $config['entity_handler']['entity_saver']);
+
+        if (null === $config['entity_handler']['criteria_resolver']) {
+            $services->set(PropertyCriteriaResolver::class);
+            $config['entity_handler']['criteria_resolver'] = PropertyCriteriaResolver::class;
+        }
+
+        $services->alias('trollbus.entity_handler.default_criteria_resolver', $config['entity_handler']['criteria_resolver']);
     }
 
     private function configureDoctrineOrmBridge(NodeBuilder $config): void
@@ -244,14 +297,65 @@ final class TrollbusBundle extends AbstractBundle
                         ->end()
                     ->stringNode('manager')
                         ->cannotBeEmpty()
-                        ->defaultNull();
+                        ->defaultNull()
+                        ->end()
+                    ->booleanNode('entity_saver_flush')
+                        ->defaultFalse()
+                        ->end()
+                    ->booleanNode('flusher')
+                        ->defaultTrue();
     }
 
     /**
      * @psalm-param Config $config
      */
-    private function loadDoctrineOrmBridge(array $config, ServicesConfigurator $services): void
+    private function loadDoctrineOrmBridge(array &$config, ServicesConfigurator $services): void
     {
-        // todo
+        if (false === $config['doctrine_orm_bridge']['enabled']) {
+            return;
+        }
+
+        if (false === InstalledVersions::isInstalled('trollbus/doctrine-orm-bridge', false)) {
+            throw new LogicException('Package "trollbus/doctrine-orm-bridge" is not installed.');
+        }
+
+        if ($config['transaction']['enabled'] && null === $config['transaction']['transaction_provider']) {
+            $services
+                ->set(DoctrineTransactionProvider::class)
+                    ->args([
+                        service($config['doctrine_orm_bridge']['manager_registry']),
+                        $config['doctrine_orm_bridge']['manager'],
+                    ]);
+            $config['transaction']['transaction_provider'] = DoctrineTransactionProvider::class;
+        }
+
+        if ($config['entity_handler']['enabled']) {
+            if (null === $config['entity_handler']['entity_finder']) {
+                $services->set(DoctrineEntityFinder::class)
+                    ->args([
+                        service($config['doctrine_orm_bridge']['manager_registry']),
+                    ]);
+                $config['entity_handler']['entity_finder'] = DoctrineEntityFinder::class;
+            }
+
+            if (null === $config['entity_handler']['entity_saver']) {
+                $services->set(DoctrineEntitySaver::class)
+                    ->args([
+                        service($config['doctrine_orm_bridge']['manager_registry']),
+                        $config['doctrine_orm_bridge']['entity_saver_flush'],
+                    ]);
+                $config['entity_handler']['entity_saver'] = DoctrineEntitySaver::class;
+            }
+        }
+
+        if ($config['doctrine_orm_bridge']['flusher']) {
+            $services
+                ->set(FlusherMiddleware::class)
+                ->args([
+                    service($config['doctrine_orm_bridge']['manager_registry']),
+                    $config['doctrine_orm_bridge']['manager'],
+                ])
+                ->tag('trollbus.middleware', ['priority' => 500]);
+        }
     }
 }
