8bdd0a29d3ddedb342f188bcae9f2d3b2293f13d
Kenny1911 <o-muzyka@mail.ru>
2025-03-08 20:34:11 +0400

    Reformat code

--- src/TrollbusBundle/TrollbusBundle.php
+++ src/TrollbusBundle/TrollbusBundle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Trollbus\TrollbusBundle;
 
-use Composer\InstalledVersions;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
--- tests/MessageBus/MessageBusTestCases/SimpleMessage/SimpleMessageManager.php
+++ tests/MessageBus/MessageBusTestCases/SimpleMessage/SimpleMessageManager.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Trollbus\Tests\MessageBus\MessageBusTestCases\SimpleMessage;
 
-use Trollbus\Tests\MessageBus\MessageBusTestCases\SimpleMessage\SimpleMessage;
-use Trollbus\Tests\MessageBus\MessageBusTestCases\SimpleMessage\SimpleMessageResult;
-
 final class SimpleMessageManager
 {
     public function handleMessage(SimpleMessage $message): SimpleMessageResult
