db76f9de80b640e76c36c8036220b39f660754e1
Kenny1911 <o-muzyka@mail.ru>
2025-03-09 19:45:20 +0400

    Add DoctrineORMBridge marker class

---
+++ src/DoctrineORMBridge/DoctrineORMBridge.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trollbus\DoctrineORMBridge;
+
+/**
+ * Aa marker class, indicated, that "trollbus/doctrine-orm-bridge" package is installed.
+ */
+final class DoctrineORMBridge
+{
+    private function __construct() {}
+}
