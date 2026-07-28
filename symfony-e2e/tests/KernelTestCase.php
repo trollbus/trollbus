e5d884736e1ea74e05e6d3a2d6ec50b99330ef90
Kenny1911 <o-muzyka@mail.ru>
2026-07-29 01:25:56 +0400

    Add new methods to symfony-e2e KernelTestCase

--- symfony-e2e/tests/KernelTestCase.php
+++ symfony-e2e/tests/KernelTestCase.php
@@ -4,15 +4,56 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Trollbus\MessageBus\MessageBus;
 
 abstract class KernelTestCase extends \Symfony\Bundle\FrameworkBundle\Test\KernelTestCase
 {
-    final public static function getMessageBus(): MessageBus
+    final protected function bootKernelAndInitDb(): void
+    {
+        self::bootKernel();
+        self::updateSchema();
+    }
+
+    final protected static function getMessageBus(): MessageBus
     {
         $bus = self::getContainer()->get(MessageBus::class);
         \assert($bus instanceof MessageBus);
 
         return $bus;
+    }
+
+    final protected static function getDoctrine(): ManagerRegistry
+    {
+        $doctrine = self::getContainer()->get('doctrine');
+        \assert($doctrine instanceof ManagerRegistry);
+
+        return $doctrine;
+    }
+
+    final protected function updateSchema(): void
+    {
+        self::executeCliCommand(['doctrine:schema:update', '--force']);
+    }
+
+    /**
+     * @param non-empty-list<non-empty-string> $argv
+     */
+    final protected static function executeCliCommand(array $argv): void
+    {
+        $kernel = self::$kernel;
+
+        if (null === $kernel) {
+            throw new \LogicException('Kernel was not booted');
+        }
+
+        $app = new Application($kernel);
+        $app->setAutoExit(false);
+        $app->setCatchExceptions(false);
+        $app->setCatchErrors(false);
+        $app->run(new ArgvInput(['', ...$argv]), new NullOutput());
     }
 }
